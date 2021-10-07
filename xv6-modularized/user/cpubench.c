3e6981b73af6e9dfcd374fd55d6ac1e8f21cd148
Iván Renison <ivan.renison@mi.unc.edu.ar>
2021-10-07 18:25:59 -0300

    Corregir error MFLOPT → KFLOPT

--- xv6-modularized/user/cpubench.c
+++ xv6-modularized/user/cpubench.c
@@ -47,7 +47,7 @@ main(int argc, char *argv[])
     int end = uptime();
     int elapsed = end - start;
     if (elapsed >= MINTICKS) {
-      printf(1, "%d: %d MFLOPT\n", pid, (int) ((ops / 1000) / elapsed));
+      printf(1, "%d: %d KFLOPT\n", pid, (int) ((ops / 1000) / elapsed));
 
       start = end;
       ops = 0;
