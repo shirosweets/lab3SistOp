a63ffbe1e4cc7769e7055f1cd073032fa98077b4
=Ivan_Renison <ivan.renison@mi.unc.edu.ar>
2021-10-08 19:53:41 -0300

    for principal de generación de los archivos para las mediciones

--- Automatizar_mediciones/AutoMed.sh
+++ Automatizar_mediciones/AutoMed.sh
@@ -1,12 +1,20 @@
 
-xs=("iobench > 1io_io1.txt &" "iobench > 1io1cpu_io1.txt & \n cpubench > 1io1cpu_cpu1.txt")
+xs=(
+    "iobench > 1io_io1.txt &"
+    "iobench > 1io1cpu_io1.txt & \n cpubench > 1io1cpu_cpu1.txt &"
+)
 
-for x in $xs;
-    do
-    gnome-terminal -e "bash -c '(sleep 1 ; echo \"$x\") | make qemu'"
-    sleep 15 #301 # 1+5*60
-    pkill qemu
-done;
+(
+    for i in 0 1;
+        do
+        echo -e "${xs[i]}"
+        (sleep 2; echo -e "${xs[i]}") | make qemu-nox > output.temp &
+        sleep 2 #302 # 2+5*60
+        pkill qemu
+    done;
+) > output.temp
+
+rm output.temp
 
 
 
