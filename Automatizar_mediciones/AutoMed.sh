80bfece537a92200b1cd85b5bea08c1c8a2fbdff
Iván Renison <ivan.renison@mi.unc.edu.ar>
2021-10-10 10:48:11 -0300

    Correcciones de estilo

--- Automatizar_mediciones/AutoMed.sh
+++ Automatizar_mediciones/AutoMed.sh
@@ -1,23 +1,21 @@
-
-# Lista generada con Generador_listas.hs
-xs=(
-    "iobench > 0cpu1io_iobench1.txt &\n"
-    "iobench > 0cpu2io_iobench1.txt &\niobench > 0cpu2io_iobench2.txt &\n"
-    "cpubench > 1cpu0io_cpubench1.txt &\n"
-    "cpubench > 1cpu1io_cpubench1.txt &\niobench > 1cpu1io_iobench1.txt &\n"
-    "cpubench > 1cpu2io_cpubench1.txt &\niobench > 1cpu2io_iobench1.txt &\niobench > 1cpu2io_iobench2.txt &\n"
-    "cpubench > 2cpu0io_cpubench1.txt &\ncpubench > 2cpu0io_cpubench2.txt &\n"
-    "cpubench > 2cpu1io_cpubench1.txt &\ncpubench > 2cpu1io_cpubench2.txt &\niobench > 2cpu1io_iobench1.txt &\n"
-    "cpubench > 2cpu2io_cpubench1.txt &\ncpubench > 2cpu2io_cpubench2.txt &\niobench > 2cpu2io_iobench1.txt &\niobench > 2cpu2io_iobench2.txt &\n"
+# Lista generada con Generador_listas.hs (comandos_test)
+tests=(
+  "iobench > 0cpu1io_iobench1.txt &\n"
+  "iobench > 0cpu2io_iobench1.txt &\niobench > 0cpu2io_iobench2.txt &\n"
+  "cpubench > 1cpu0io_cpubench1.txt &\n"
+  "cpubench > 1cpu1io_cpubench1.txt &\niobench > 1cpu1io_iobench1.txt &\n"
+  "cpubench > 1cpu2io_cpubench1.txt &\niobench > 1cpu2io_iobench1.txt &\niobench > 1cpu2io_iobench2.txt &\n"
+  "cpubench > 2cpu0io_cpubench1.txt &\ncpubench > 2cpu0io_cpubench2.txt &\n"
+  "cpubench > 2cpu1io_cpubench1.txt &\ncpubench > 2cpu1io_cpubench2.txt &\niobench > 2cpu1io_iobench1.txt &\n"
+  "cpubench > 2cpu2io_cpubench1.txt &\ncpubench > 2cpu2io_cpubench2.txt &\niobench > 2cpu2io_iobench1.txt &\niobench > 2cpu2io_iobench2.txt &\n"
 )
 
 (
-    for i in {0..7}; do
-        (sleep 2; echo -e "${xs[i]}") | make qemu-nox CPUS=1 > /dev/null &
-        sleep 302 # 2+5*60
-        pkill qemu
-    done;
+  for i in {0..7}; do
+    (sleep 2; echo -e "${tests[i]}") | make qemu-nox CPUS=1 > /dev/null &
+    sleep 302 # 2+5*60
+    pkill qemu
+  done;
 ) > /dev/null
-
 
 
--- Automatizar_mediciones/Generador_listas.hs
+++ Automatizar_mediciones/Generador_listas.hs
@@ -1,4 +1,3 @@
-
 -- Funciones de fmt
 
 (+|) :: Show a => String -> a -> String
@@ -17,21 +16,22 @@ tests = [(cpu, io) | cpu <- [0..2], io <- [0..2]]
 -- Comando para pasarle a xv6 para ese caso de test
 comandos :: (Word, Word) -> String
 comandos (cpu, io) = unlines $
-    ["cpubench > "+| cpu |+"cpu"+| io |+"io_cpubench" +| ncpu |+".txt &"
-        | ncpu <- [1..cpu]
-    ]
-    ++ ["iobench > "+| cpu |+"cpu"+| io |+"io_iobench" +| nio |+".txt &"
-        | nio <- [1..io]
-    ]
+  ["cpubench > "+| cpu |+"cpu"+| io |+"io_cpubench"+| ncpu |+".txt &"
+    | ncpu <- [1..cpu]
+  ]
+  ++ ["iobench > "+| cpu |+"cpu"+| io |+"io_iobench"+| nio |+".txt &"
+    | nio <- [1..io]
+  ]
 
+-- Archivos generados dentro de xv6 en este caso del test
 archivos :: (Word, Word) -> [String]
 archivos (cpu, io) =
-    [""+| cpu |+"cpu"+| io |+"io_cpubench" +| ncpu |+".txt"
-        | ncpu <- [1..cpu]
-    ]
-    ++ [""+| cpu |+"cpu"+| io |+"io_iobench" +| nio |+".txt"
-        | nio <- [1..io]
-    ]
+  [""+| cpu |+"cpu"+| io |+"io_cpubench"+| ncpu |+".txt"
+    | ncpu <- [1..cpu]
+  ]
+  ++ [""+| cpu |+"cpu"+| io |+"io_iobench"+| nio |+".txt"
+    | nio <- [1..io]
+  ]
 
 
 
