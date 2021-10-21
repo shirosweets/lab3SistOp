37eb70c4a850104ca0550463554fece4a824367d
Iván Renison <ivan.renison@mi.unc.edu.ar>
2021-10-21 15:30:27 -0300

    Corregir error de caso de comando de mas 100 caracteres

--- Automatizar_mediciones/AutoMed.sh
+++ Automatizar_mediciones/AutoMed.sh
@@ -1,19 +1,19 @@
 # Lista generada con Generador_listas.hs (comandos_test)
 tests=(
   "iobench > 0cpu1io_io1 &"
-  "iobench > 0cpu2io_io1 & ; iobench > 0cpu2io_io2 &"
+  "iobench > 0cpu2io_io1 &\niobench > 0cpu2io_io2 &"
   "cpubench > 1cpu0io_cpu1 &"
-  "cpubench > 1cpu1io_cpu1 & ; iobench > 1cpu1io_io1 &"
-  "cpubench > 1cpu2io_cpu1 & ; iobench > 1cpu2io_io1 & ; iobench > 1cpu2io_io2 &"
-  "cpubench > 2cpu0io_cpu1 & ; cpubench > 2cpu0io_cpu2 &"
-  "cpubench > 2cpu1io_cpu1 & ; cpubench > 2cpu1io_cpu2 & ; iobench > 2cpu1io_io1 &"
-  "cpubench > 2cpu2io_cpu1 & ; cpubench > 2cpu2io_cpu2 & ; iobench > 2cpu2io_io1 & ; iobench > 2cpu2io_io2 &"
+  "cpubench > 1cpu1io_cpu1 &\niobench > 1cpu1io_io1 &"
+  "cpubench > 1cpu2io_cpu1 &\niobench > 1cpu2io_io1 &\niobench > 1cpu2io_io2 &"
+  "cpubench > 2cpu0io_cpu1 &\ncpubench > 2cpu0io_cpu2 &"
+  "cpubench > 2cpu1io_cpu1 &\ncpubench > 2cpu1io_cpu2 &\niobench > 2cpu1io_io1 &"
+  "cpubench > 2cpu2io_cpu1 &\ncpubench > 2cpu2io_cpu2 &\niobench > 2cpu2io_io1 &\niobench > 2cpu2io_io2 &"
 )
 
 # Ejecutar desde carpeta xv-modularized y después de hacer make qemu
 (
   for i in {0..7}; do
-    (sleep 5; echo "${tests[i]}") | timeout 305 make qemu-nox CPUS=1
+    (sleep 2; echo -e "${tests[i]}") | timeout 302 make qemu-nox CPUS=1
   done;
 )
 
